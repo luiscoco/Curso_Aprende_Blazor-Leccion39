693cbdbb2ebaa2b76955266a11218b181fb88e7c
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-09-12 16:37:16 +0200

    Agregar archivos de proyecto.

---
+++ wwwroot/js/example.js
@@ -0,0 +1,18 @@
+﻿window.ShowSwal = function (type, message) {
+    if (type == "success")
+    {
+        Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success"
+        });
+    }
+    if (type == "error") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    }
+}
